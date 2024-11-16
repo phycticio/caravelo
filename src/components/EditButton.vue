26fbae8bef7b2d2ecbf237eadf9df109c5fc9e27
Javier Troya <javiertroya@gmail.com>
2024-11-16 16:38:21 +0100

    First commit with partial modal design.

---
+++ src/components/EditButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-dialog max-width="670">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        v-bind="activatorProps"
+        color="surface-variant"
+        text="Edit Flights"
+        variant="outlined"
+        prepend-icon="mdi-pencil"
+      ></v-btn>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card>
+        <v-card-title>
+          Edit Flights
+        </v-card-title>
+        <v-card-subtitle>
+          Add or remove flights from the subscriber
+        </v-card-subtitle>
+
+        <v-container>
+          <v-row>
+            <v-col md="4">
+              <v-text-field>
+                <template v-slot:append>
+                  <v-icon>
+                    mdi-plus-circle-outline
+                  </v-icon>
+                </template>
+                <template v-slot:prepend>
+                  <v-icon>
+                    mdi-minus-circle-outline
+                  </v-icon>
+                </template>
+              </v-text-field>
+            </v-col>
+            <v-col md="8">
+              Como estas?
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            text="Close Dialog"
+            @click="isActive.value = false"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-main class="d-flex align-center justify-center" style="min-height: 100vh;">
+      <EditButton />
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+  import EditButton from "@/components/EditButton.vue";
+</script>
